bef5a3b00a9a77351a9ac797d71b0a73867862b4
Carlos Sandoval <csandovalc@unal.edu.co>
2022-06-18 23:45:02 -0500

    Linked list test done

--- src/public/Structures/Test/Lineal/LikedList.test.js
+++ src/public/Structures/Test/Lineal/LikedList.test.js
@@ -1,4 +1,5 @@
 const LinkedList = require("../../Implementations/Lineal/LinkedList");
+const LinealNode = require("../../Implementations/Lineal/LinealNode");
 
 let list = new LinkedList();
 
@@ -80,4 +81,74 @@ test("Exits? (Existing element)", () => {
 
 test("Exits? (Not existing element)", () => {
 	expect(list.exists("t")).toBeFalsy();
+});
+
+test("Get Node with valid index", () => {
+	const node = list.getNode(2);
+	const isValidNode = node instanceof LinealNode;
+	expect(isValidNode && node.data === "e").toBeTruthy();
+});
+
+test("Get Node with invalid index", () => {
+	expect(() => {
+		list.getNode(list.size);
+	}).toThrow("INVALID POSITION");
+});
+
+test("Erase with data value (existing node)", () => {
+	list.erase("e");
+	expect(list.print()).toBe("[a, c, f]");
+});
+
+test("Erase with data value (not existing value)", () => {
+	expect(() => {
+		list.erase("n");
+	}).toThrow("NOT EXISTING DATA");
+});
+
+test("Remove node with valid index", () => {
+	list.remove(2);
+	expect(list.print()).toBe("[a, c]");
+});
+
+test("Remove node with invalid index", () => {
+	expect(() => {
+		list.remove(list.size);
+	}).toThrow("INVALID POSITION");
+});
+
+test("Add before node", () => {
+	expect(list.print()).toBe("[a, c]");
+
+	const nodeReference = list.getNode(1);
+	expect(nodeReference.data).toBe("c");
+	expect(nodeReference instanceof LinealNode).toBeTruthy();
+
+	list.addBefore(nodeReference, "v");
+	expect(list.print()).toBe("[a, v, c]");
+	expect(list.size).toBe(3);
+});
+
+test("Add before node with invalid reference", () => {
+	expect(() => {
+		list.addBefore("ANY VALUE", "e");
+	}).toThrow("INVALID NODE");
+});
+
+test("Add after node", () => {
+	expect(list.print()).toBe("[a, v, c]");
+
+	const nodeReference = list.getNode(1);
+	expect(nodeReference.data).toBe("v");
+	expect(nodeReference instanceof LinealNode).toBeTruthy();
+
+	list.addAfter(nodeReference, "e");
+	expect(list.print()).toBe("[a, v, e, c]");
+	expect(list.size).toBe(4);
+});
+
+test("Add after node with invalid reference", () => {
+	expect(() => {
+		list.addAfter("ANY VALUE", "l");
+	}).toThrow("INVALID NODE");
 });
